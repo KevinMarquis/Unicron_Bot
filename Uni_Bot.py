baf30cf6ce1de699b8b2c81d9196ab8cc7fb654b
Kevin Marquis <kevin.marquis1@gmail.com>
2022-10-27 22:39:40 -0400

    Basic functionality, including help command

--- Uni_Bot.py
+++ Uni_Bot.py
@@ -22,50 +22,76 @@ logger.addHandler(handler)
 """SETUP"""
 
 ###VARIABLE INITIALIZATION###
-token = ''  #TODO
+intents = discord.Intents.all()
+# intents.message_content = True
+token = 'MTAzNTM2MjQyOTc1ODA4MzA3Mg.G8JInH.FwQS46hqeYwMkl28qWOzKgWsY66n_elx-W1Xpg'
 default_prefix = "!"
 prefix = default_prefix  # We will keep prefix/user data as instance data for now - this will be changed to be per server
-bot = commands.Bot(command_prefix='$')
+bot = commands.Bot(command_prefix='$', intents=intents)
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = discord.Client(intents=intents)
 
-@client.event
-async def on_ready():
-    print(f'We have logged in as {client.user}')
 
-''' - May not need this
-@client.event
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+
+# - May not need this
+@bot.event
 async def on_message(message):
-    if message.author == client.user:
+    if message.author == bot.user:
         return
 
-    if message.content.startswith('$hello'):
+    if message.content.startswith('&&&hello'):
         await message.channel.send('Hello!')
-'''
+    await bot.process_commands(message)
+
 
 """FUNCTIONS"""
-@bot.command()
-async def help(ctx):
-    message_release = "------HELP------\n"
-    message_release += prefix + "help - Prints out all commands\n"
-    message_release += prefix + "prefix _ - Sets the prefix to be the inputted character(s)\n"
-    ctx.send(message_release)
+
+
+class MyHelpCommand(commands.MinimalHelpCommand):
+    async def send_pages(self):
+        destination = self.get_destination()
+        e = discord.Embed(color=discord.Color.blurple(), description='')
+        for page in self.paginator.pages:
+            e.description += page
+        await destination.send(embed=e)
+
+
+bot.help_command = MyHelpCommand()
+
 
 @bot.command()
-def pref_change(ctx, new_pref):
+async def hello(ctx):
+    await ctx.send(ctx.author.mention + " hello!")
+
+
+@bot.command(name="test")
+async def test(ctx):
+    await ctx.send("Test")
+
+
+@bot.command()
+async def test2(ctx, arg):
+    await ctx.send(arg)
+
+
+@bot.command(name="prefix_change")
+async def pref_change(ctx, new_pref):
     """Changes the bot's command prefix to the given parameter"""
-    nonlocal prefix
+    global prefix
     prefix = new_pref
     bot.command_prefix = prefix
-    ctx.send("Prefix has been set to: " + prefix)
+    await ctx.send("Prefix has been set to: " + prefix)
 
 
 """STARTUP"""
 # Assume client refers to a discord.Client subclass...
 # Suppress the default configuration since we have our own
-client.run(token, log_handler=None)
-
-
+# client.run(token, log_handler=None)
+bot.run(token)
